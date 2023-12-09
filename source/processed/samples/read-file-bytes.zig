3ca28f6b69be1f5ce29a643d868f93e233dd5f10
Renato Athaydes <renato@athaydes.com>
2023-12-09 10:24:50 +0100

    Added read file bytes sample.

---
+++ source/processed/samples/read-file-bytes.zig
@@ -0,0 +1,26 @@
+// {{ define name "Read file bytes" }}{{ define id "read-file-bytes" }}{{ eval name }}
+const std = @import("std");
+
+// Sample starts here{{ slot contents }}\
+test "Read bytes from a file" {
+    // current working directory
+    const cwd = std.fs.cwd();
+
+    const handle = cwd.openFile("hello.txt",
+    // this is the default, so could be just '.{}'
+    .{ .mode = .read_only }) catch {
+        // file not found
+        return;
+    };
+    defer handle.close();
+
+    // read into this buffer
+    var buffer: [64]u8 = undefined;
+    const bytes_read = handle.readAll(&buffer) catch unreachable;
+
+    // if bytes_read is smaller than buffer.len, then EOF was reached
+    try std.testing.expectEqual(@as(usize, 6), bytes_read);
+
+    const expected_bytes = [_]u8{ 'h', 'e', 'l', 'l', 'o', '\n' };
+    try std.testing.expectEqualSlices(u8, &expected_bytes, buffer[0..bytes_read]);
+}
