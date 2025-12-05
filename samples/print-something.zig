9687e35b0f491d378d2a15a410e54890fe907f40
renatoathaydes <renatoathaydes@hotmail.com>
2025-12-05 18:38:00 +0000

    Deploying to gh-pages from @ renatoathaydes/zig-common-tasks@bbb6124c336a285e561d21f2cd4cf78f71d5354d 🚀

--- samples/print-something.zig
+++ samples/print-something.zig
@@ -1,22 +1,32 @@
 // Print something to stdout/stderr
 const std = @import("std");
 
-// Sample starts herepub fn main() !void {
+pub fn main() !void {
+    hello_world();
+}
+
+// Sample starts herepub fn hello_world() !void {
+    // to print something for debugging purposes, there's an easy way!
+    std.debug.print("Hello world!\n", .{});
+
+    // but on real apps, you will want to use the "real" stdout/stderr
+
     var buffer: [1024]u8 = undefined;
 
-    // Use File.stdout() or File.stderr()
-    var writer = std.fs.File.stdout().writer(&buffer);
-    const stdout = &writer.interface;
+    // Choose File.stdout() or File.stderr()
+    // Note: don't use stdout() in tests, it will block your test on "zig build test"!
+    var writer = std.fs.File.stderr().writer(&buffer);
+    const stderr = &writer.interface;
 
-    try stdout.print("Hello world!\n", .{});
+    try stderr.print("Hello world!\n", .{});
 
     // you can provide arguments, similar to C's printf,
     // though in Zig the format is checked at compile-time!
-    try stdout.print("number: {d}, string: {s}\n", .{ 42, "fourty-two" });
+    try stderr.print("number: {d}, string: {s}\n", .{ 42, "fourty-two" });
 
-    try stdout.flush(); // Don't forget to flush!
+    try stderr.flush(); // Don't forget to flush!
 } //  Sample ends
 
 test "check main" {
-    try main();
+    try hello_world();
 }
